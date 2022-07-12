843cbeec29860bd063ff697ce56fd0bd395a368f
ikyrie <mah.c5r@gmail.com>
2022-07-12 17:04:24 -0300

    Removing dumb imports

--- lib/components/sections/actions.dart
+++ lib/components/sections/actions.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/foundation/key.dart';
-import 'package:flutter/src/widgets/framework.dart';
 
 import '../activity.dart';
 import '../box_card.dart';
--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,9 +1,6 @@
-import 'package:estilizacao_componentes/components/activity.dart';
-import 'package:estilizacao_componentes/components/box_card.dart';
 import 'package:estilizacao_componentes/components/sections/actions.dart';
 import 'package:estilizacao_componentes/components/sections/header.dart';
 import 'package:estilizacao_componentes/components/sections/recent_activities.dart';
-import 'package:estilizacao_componentes/components/status.dart';
 import 'package:flutter/material.dart';
 
 class Home extends StatelessWidget {
